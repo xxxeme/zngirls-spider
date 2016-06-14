c1cca2871db157d9677d2b564ac9b5735eb879a0
Wei Wei <lyweiwei@outlook.com>
2016-06-14 18:49:29 +0800

    get the basic command work

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
-var path = require('path');
+// var path = require('path');
 var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 var excludeGitignore = require('gulp-exclude-gitignore');
 var mocha = require('gulp-mocha');
 var istanbul = require('gulp-istanbul');
-var nsp = require('gulp-nsp');
+// var nsp = require('gulp-nsp');
 var plumber = require('gulp-plumber');
 var babel = require('gulp-babel');
 var del = require('del');
@@ -15,19 +15,19 @@ var isparta = require('isparta');
 require('babel-core/register');
 
 gulp.task('static', function () {
-  return gulp.src('**/*.js')
+  return gulp.src(['**/*.js', '!./lib/bin/**/*.js'])
     .pipe(excludeGitignore())
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
 });
 
-gulp.task('nsp', function (cb) {
-  nsp({package: path.resolve('package.json')}, cb);
-});
+// gulp.task('nsp', function (cb) {
+//   nsp({package: path.resolve('package.json')}, cb);
+// });
 
 gulp.task('pre-test', function () {
-  return gulp.src('lib/**/*.js')
+  return gulp.src(['lib/**/*.js', '!lib/bin/**/*.js'])
     .pipe(excludeGitignore())
     .pipe(istanbul({
       includeUntested: true,
@@ -57,7 +57,9 @@ gulp.task('watch', function () {
 
 gulp.task('babel', ['clean'], function () {
   return gulp.src('lib/**/*.js')
-    .pipe(babel())
+    .pipe(babel({
+      presets: ['es2015']
+    }))
     .pipe(gulp.dest('dist'));
 });
 
@@ -65,5 +67,8 @@ gulp.task('clean', function () {
   return del('dist');
 });
 
-gulp.task('prepublish', ['nsp', 'babel']);
+gulp.task('prepublish', [
+  // 'nsp',
+  'babel'
+]);
 gulp.task('default', ['static', 'test']);
